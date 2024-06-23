26dbf6c95501ea3227e1d8adeb06b45f9b4bb0cc
Young Jin <likeRudin@gmail.com>
2024-06-23 21:42:46 +0900

    [Feature] complement api fetchers: add type and Story

--- src/apis/heros.ts
+++ src/apis/heros.ts
@@ -1,3 +1,11 @@
+import type {
+  HerosApiResponse,
+  ComicApiResponse,
+  SeriesApiResponse,
+  StoryApiResponse,
+  EventApiResponse,
+} from "../types";
+
 export const fetchHeros = async () => {
   const json = await (
     await fetch(
@@ -5,11 +13,11 @@ export const fetchHeros = async () => {
     )
   ).json();
 
-  return json.data.result;
+  return (json as HerosApiResponse).data.results;
 };
 
 export const fetchOneHeroDetails = async (id: number) => {
-  const [comicJson, seriesJson, eventJson] = await Promise.all([
+  const [comicJson, seriesJson, eventJson, storyJson] = await Promise.all([
     fetch(
       `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters/${id}/comics`
     ).then((res) => res.json()),
@@ -19,11 +27,15 @@ export const fetchOneHeroDetails = async (id: number) => {
     fetch(
       `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters/${id}/events`
     ).then((res) => res.json()),
+    fetch(
+      `https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters/${id}/stories`
+    ).then((res) => res.json()),
   ]);
 
   return {
-    comics: comicJson.data.results,
-    series: seriesJson.data.results,
-    events: eventJson.data.results,
+    comics: (comicJson as ComicApiResponse).data.results,
+    series: (seriesJson as SeriesApiResponse).data.results,
+    events: (eventJson as EventApiResponse).data.results,
+    stories: (storyJson as StoryApiResponse).data.results,
   };
 };
