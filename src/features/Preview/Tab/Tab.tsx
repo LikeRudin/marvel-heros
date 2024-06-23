950bd1d80014ef7d3354e007ac09285cbd04ed3e
Young Jin <likeRudin@gmail.com>
2024-06-24 01:07:38 +0900

    [Feature] add Feature Component Tab :  show basic info of heros

---
+++ src/features/Preview/Tab/Tab.tsx
@@ -0,0 +1,46 @@
+import { Comics, Events, Series, Stories } from "../../../types";
+import { TabNamesType } from "../Preview";
+
+import styles from "./Tab.module.css";
+
+interface ITabProps {
+  tabName: TabNamesType;
+  comics: Comics;
+  series: Series;
+  events: Events;
+  stories: Stories;
+}
+
+const Tab = ({ tabName, comics, series, events, stories }: ITabProps) => {
+  let items = [];
+  switch (tabName) {
+    case "Comics":
+      items = comics.items;
+      break;
+    case "Series":
+      items = series.items;
+      break;
+    case "Events":
+      items = events.items;
+      break;
+    case "Stories":
+      items = stories.items;
+      break;
+  }
+
+  return (
+    <div>
+      <ul className={styles.wrapper}>
+        {items.length > 0
+          ? items.map((item, index) => (
+              <li key={index} className={styles.listItem}>
+                {item.name}
+              </li>
+            ))
+          : ""}
+      </ul>
+    </div>
+  );
+};
+
+export default Tab;
